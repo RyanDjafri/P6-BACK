ceda9d431d002ed498c7dedf084347c29dad2770
RyanDjafri <ryandjafri@gmail.com>
2022-10-26 12:00:27 +0200

    ajout helmet

--- index.js
+++ index.js
@@ -4,12 +4,33 @@ const { authRouter } = require("./routes/auth");
 const port = 3000;
 const path = require("path");
 const bodyParser = require("body-parser");
-
+const helmet = require("helmet");
 // Connection to database
 require("./db/mongo");
 
 // Middleware
 app.use(bodyParser.json());
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: false,
+    frameguard: false,
+  })
+);
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/sauces", saucesRouter);
 app.use("/api/auth", authRouter);
 
